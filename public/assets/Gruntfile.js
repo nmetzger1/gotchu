f69dc974eae316ef2179e7be841cf7d43f0e57c5
AndyEm <andyem64@gmail.com>
2017-05-18 21:17:28 -0400

    moved sass files into assets

---
+++ public/assets/Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                options: {
+                    outputStyle: 'compressed'
+                },
+                files: {
+                    //Where it complies to, Watches
+                    'css/main.css': 'scss/main.scss'
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: '**/*.scss',
+                tasks: ['sass']
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['watch']);
+};
