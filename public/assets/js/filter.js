8555c7c2730b1447a5d6ed8a14028059b7aa7f74
AndyEm <andyem64@gmail.com>
2017-05-31 19:23:52 -0400

    filter by description

---
+++ public/assets/js/filter.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $("#search").on('keyup', function(event){
+        filter();
+    });
+    // Declare variables
+    function filter () {
+        var input, filter, post, a, i;
+        input = document.getElementById('search');
+        filter = input.value.toUpperCase();
+        post = document.getElementsByClassName('postings');
+        // Loop through all list items, and hide those who don't match the search           query
+        for (i = 0; i < post.length; i++) {
+            a = post[i].getElementsByTagName("p")[0];
+            if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                post[i].style.display = "";
+            } else {
+                post[i].style.display = "none";
+            }
+        }
+    }
+
+
+});
