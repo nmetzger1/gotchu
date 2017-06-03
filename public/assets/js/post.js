0015f566d3567bea29d909792005e502b14cca0a
AndyEm <andyem64@gmail.com>
2017-06-02 21:22:38 -0400

    random images

--- public/assets/js/post.js
+++ public/assets/js/post.js
@@ -1,6 +1,18 @@
 var newPost = {};
 
 $(document).ready(function () {
+    // Image
+    var imgArr = ['http://s.eatthis-cdn.com/media/images/ext/543627202/happy-people-friends.jpg',
+        'http://i.huffpost.com/gen/3872496/images/n-HAPPY-628x314.jpg',
+        'http://st.motortrend.com/uploads/sites/5/2017/04/2017-Honda-Civic-Si-Coupe-cabin-1.jpg',
+        'https://secure-akns.subaru.com/content/media/mp_hero_880/brz-17-sports-car-seating.jpg',
+        'http://cdn.attackofthecute.com/September-21-2011-22-10-11-6765.jpeg',
+        'http://centeredmanproject.com/wp-content/uploads/2016/05/learn-from-conor-mcgregor-centeredmanproject.jpg',
+        'http://homestead-and-survival.com/wp-content/uploads/2015/11/2-diy-bow-and-arrow-projects-for-survival.jpg',
+        'https://thumbs.dreamstime.com/z/happy-farmers-standing-arms-crossed-sunny-day-55571735.jpg',
+        'https://eatdrinkbetter.com/wp-content/uploads/2014/08/Happy-Farmer.jpg',
+        'https://sg-dae.kxcdn.com/blog/wp-content/uploads/2014/05/Hands-of-farmers-family-holdin-sapling.jpg',
+        'http://www.meh.ro/original/2010_03/meh.ro3660.jpg'];
     //Getting references to the post input and post container
 
     // Getting the initial list of Posts
@@ -44,11 +56,15 @@ $(document).ready(function () {
 
     // // Function for creating a new list row for posts
     function createPostDiv(postData) {
+    // RandomNuber
+        var randNum = Math.floor(Math.random() * 10);
 
         var postId = "post" + postData.id;
         var Post = $('<div class="postings">');
         Post.attr('id', postId);
-        Post.append('<img src="https://s-media-cache-ak0.pinimg.com/originals/28/81/4d/28814dbf59005e2f4953ee62f76df0b6.jpg" width="250px">');
+
+        Post.append('<img src= ' + imgArr[randNum] + ' +  width="250px">');
+        // Post.append('<img src="https://s-media-cache-ak0.pinimg.com/originals/28/81/4d/28814dbf59005e2f4953ee62f76df0b6.jpg" width="250px">');
         Post.append("<p>" + postData.title + "</p>");
         Post.append("<p>" + postData.category + "</p>");
         Post.append("<p>" + postData.distance  + "</p>");
